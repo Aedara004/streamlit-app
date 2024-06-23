2e0deeeade280009e0e45d90506eece4a2b941f8
Aedara004 <Aedara@charlotte.edu>
2024-06-22 20:11:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if selected_category:
             for subcategory in selected_subcategories:
                 subcategory_data = filtered_data[filtered_data['Sub_Category'] == subcategory]
                 
-                st.write(subcategory_data.set_index('Sub_Category')['Quantity'])
+                #st.write(subcategory_data.set_index('Sub_Category')['Quantity'])
             
                 st.line_chart(subcategory_data.set_index('Sub_Category')['Quantity'])
             
