f200454e3d23f86591a7ba9ce4fbb5a8cfaeb118
Aedara004 <Aedara@charlotte.edu>
2024-06-27 00:59:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ st.bar_chart(df, x="Category", y="Sales")
 #category = st.selectbox("Select a Category", df['Category'].unique())
 #x=df.groupby("Category").sum()
 #st.dataframe(x.loc[category])
+st.write("## Your additions")
+st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
 
 # Calculate profit and profit margin
 df['Profit'] = df['Sales'] - df['Discount']
@@ -45,6 +47,7 @@ if selected_category:
 
     # Multi-select for subcategories
     selected_subcategories = st.multiselect('Select subcategories', subcategories)
+    st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
 
     if selected_subcategories:
         st.write(f"Selected subcategories for Category '{selected_category}': {selected_subcategories}")
@@ -60,6 +63,7 @@ if selected_category:
                 subcategory_data = filtered_data[filtered_data['Sub_Category'] == subcategory]
 
                 #st.write(subcategory_data.set_index('Sub_Category')['Quantity'])
+                st.write("### (3) show a line chart of sales for the selected items in (2)")
 
                 st.line_chart(subcategory_data.set_index('Sub_Category')['Sales'])
 
@@ -86,6 +90,8 @@ else:
     st.write("Please select a category.")
 
 # Using as_index=False here preserves the Category as a column.  If we exclude that, Category would become the datafram index and we would need to use x=None to tell bar_chart to use the index
+st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
+
 st.bar_chart(df.groupby("Category", as_index=False).sum(), x="Category", y="Sales", color="#04f")
 #st.bar_chart(df.groupby(x.loc[category], as_index=False).sum(), x="Category", y="Sales", color="#04f")
 # Aggregating by time
@@ -94,16 +100,11 @@ df["Order_Date"] = pd.to_datetime(df["Order_Date"])
 df.set_index('Order_Date', inplace=True)
 # Here the Grouper is using our newly set index to group by Month ('M')
 sales_by_month = df.filter(items=['Sales']).groupby(pd.Grouper(freq='M')).sum()
+st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
 
 st.dataframe(sales_by_month)
 
 # Here the grouped months are the index and automatically used for the x axis
 st.line_chart(sales_by_month, y="Sales")
 
-st.write("## Your additions")
-st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
-st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
-st.write("### (3) show a line chart of sales for the selected items in (2)")
-st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
-st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
 
